5e401026bb7c34624f677a6e63047a1e6d148f3e
LightAlykard <lightalykard@yandex.ru>
2022-07-17 23:38:15 +0300

    HW-4 v0.1
    Курса по наблюдаемости у меня не было, так что решил перебывать на приложении по проще(задание со второго урока), в итоге у меня получился мальца стремный гибрид приложения со второго урока с тем что вы показывали на уроке, но оно работает и я разобрался как добавлять и собирать метрики в приложении.
    Вопрос только такой, у меня там сервер с хендлерами сделан через echo и в нем с HandlerFunc передается только echo.Context, а для того что бы поставить span нужен Tracer.Start(ctx, "...") в котором ctx - контекст, но echo.Context в него передать нельзя, я сделал лямду функцию прям в initHandlers, но это как-то стремно, как сделать в данном случае правильно?

--- k8s-go-app/go.mod
+++ k8s-go-app/go.mod
@@ -1,18 +1,35 @@
-module k8s-go-app
+module k8s-go-opt
 
 go 1.17
 
 require (
+	github.com/LightAlykard/GoLibsTest v0.0.0-20220717185407-81827dc574c1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.7.2 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.33.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.33.0 // indirect
+	go.opentelemetry.io/otel v1.8.0 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.8.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.31.0 // indirect
+	go.opentelemetry.io/otel/metric v0.31.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.8.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.8.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 )
--- k8s-go-app/server/echo.go
+++ k8s-go-app/server/echo.go
@@ -3,14 +3,21 @@ package server
 import (
 	"context"
 	"net/http"
+	"time"
+
+	"github.com/LightAlykard/GoLibsTest/logic"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type Server struct {
 	VersionInfo
-	port string
+	port  string
+	Tr    trace.Tracer
+	Logic logic.Logic
 }
 
 type VersionInfo struct {
@@ -19,19 +26,24 @@ type VersionInfo struct {
 	Build   string
 }
 
-func New(info VersionInfo, port string) *Server {
+func New(info VersionInfo, port string, tr trace.Tracer, lg logic.Logic) *Server {
 	return &Server{
 		VersionInfo: info,
 		port:        port,
+		Tr:          tr,
+		Logic: logic.Logic{
+			Tr: tr,
+		},
 	}
 }
 
 func (s Server) Serve(ctx context.Context) error {
 	e := echo.New()
+	e.Use(otelecho.Middleware("my-server"))
 	e.HideBanner = true
 	e.Use(middleware.Recover())
 	e.Use(middleware.Recover())
-	s.initHandlers(e)
+	s.initHandlers(e, ctx)
 	go func() {
 		e.Logger.Infof("start server on port: %s", s.port)
 		err := e.Start(":" + s.port)
@@ -43,10 +55,29 @@ func (s Server) Serve(ctx context.Context) error {
 	return e.Shutdown(ctx)
 }
 
-func (s Server) initHandlers(e *echo.Echo) {
+func (s Server) initHandlers(e *echo.Echo, ctx context.Context) {
 	e.GET("/", handler)
 	e.GET("/__heartbeat_", heartbeatHandler)
 	e.GET("/__version__", s.versionHandler)
+	//e.GET("/entities", s.simpleHander2(e.Context, ctx))
+	//r.HandleFunc("/create", r.AuthMiddleware(http.HandlerFunc(r.CreateLink)).ServeHTTP)
+	//e.HandleFunc("/entities/", s.simpleHander()).Methods(http.MethodPost)
+
+	e.Any("/entities", func(c echo.Context) error {
+		ctx, span := s.Tr.Start(ctx, "foo")
+		defer span.End()
+
+		data := s.Logic.Example(ctx)
+
+		if time.Now().Second()%2 == 0 {
+			return c.String(http.StatusOK, data)
+		} else {
+			return c.String(http.StatusInternalServerError, data)
+		}
+
+		//return c.NoContent(http.StatusNotFound)
+	})
+
 	e.Any("/*", func(c echo.Context) error {
 		return c.NoContent(http.StatusNotFound)
 	})
